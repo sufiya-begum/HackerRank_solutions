de314eef2eba87c06600ac89d390ef701d61669f
sufiya-begum <sufiyabegum984@gmail.com>
2026-09-23 20:18:23 +0530

    Add CodeChef PGNATK - PGNATK

---
+++ codechef/PGNATK - PGNATK/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    long long n,k;
+    cin >> n >> k;
+    long long total_time = n + (n-1)/(k-1);
+    cout << total_time << "
+";
+}
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+
+}
+
+solve local
